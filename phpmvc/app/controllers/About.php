d0724290f5936293b82ca4613c68684d5638283d
Agung Haryanto <32664168+agungharyanto@users.noreply.github.com>
2019-04-13 13:47:30 +0700

    Add files via upload

---
+++ phpmvc/app/controllers/About.php
@@ -0,0 +1,18 @@
+<?php
+
+class About extends Controller {
+    public function index($nama = 'Agung', $pekerjaan = 'pelajar'){
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['judul'] = 'About';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
+    }
+    public function page(){
+        $data['judul'] = 'Pages';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
+    }
+}
